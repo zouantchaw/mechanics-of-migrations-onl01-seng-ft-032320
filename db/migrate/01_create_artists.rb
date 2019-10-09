81b04966d1d37606aff8cb3d69289b895ea9a05c
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-09 17:00:05 -0400

    Revert "Update 01_create_artists.rb"
    This reverts commit d19a16c8b78326a126f39db65119fe17f87788c0.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
 end
