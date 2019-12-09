0e64adadd9e42da15c24dcceec6831836705544f
maxwellbenton <maxwellbenton@gmail.com>
2019-12-09 16:58:00 -0500

    removes focus message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@ require 'rake'
 load './Rakefile'
 
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  # config.run_all_when_everything_filtered = true
+  # config.filter_run :focus
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
